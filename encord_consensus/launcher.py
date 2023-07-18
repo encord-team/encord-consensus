fdc1cc71a62dab6f427547a0d08ad488e9fcbee2
alex-encord <alexandre@encord.com>
2023-07-18 15:46:09 +0100

    Use os.path to join paths.

--- encord_consensus/launcher.py
+++ encord_consensus/launcher.py
@@ -1,3 +1,4 @@
+import os
 import site
 import sys
 
@@ -6,5 +7,5 @@ from streamlit.web import cli as stcli
 
 def launch():
     site_packages = site.getsitepackages().pop()
-    sys.argv = ["streamlit", "run", f"{site_packages}/encord_consensus/app.py"]
+    sys.argv = ["streamlit", "run", os.path.join(site_packages, 'encord_consensus', 'app.py')]
     sys.exit(stcli.main())
