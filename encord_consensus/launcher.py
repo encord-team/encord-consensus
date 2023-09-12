c09afd68de4570c65c6c99d40222d1e81b6648af
Eloy Pérez Torres <eloy@encord.com>
2023-09-12 12:07:45 +0200

    fix: incomplete start path when launching the app

--- encord_consensus/launcher.py
+++ encord_consensus/launcher.py
@@ -7,7 +7,7 @@ from encord_consensus.app.common.constants import HOME_PAGE_TITLE
 
 
 def launch():
-    streamlit_page = (Path(__file__).parent / f"app/{HOME_PAGE_TITLE}.py").expanduser().resolve()
+    streamlit_page = (Path(__file__).parent / f"app/{HOME_PAGE_TITLE.replace(' ', '_')}.py").expanduser().resolve()
     sys.argv = ["streamlit", "run", streamlit_page.as_posix()]
 
     sys.exit(stcli.main())
