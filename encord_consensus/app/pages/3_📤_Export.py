5aa216a788f8c0f9cd408bf0c130a05ccfa0b507
Eloy Pérez Torres <eloy@encord.com>
2023-09-15 15:00:56 +0200

    misc: mark bulk export feature as `in progress`

--- encord_consensus/app/pages/3_📤_Export.py
+++ encord_consensus/app/pages/3_📤_Export.py
@@ -11,4 +11,8 @@ st.set_page_config(page_title=CONSENSUS_BROWSER_TAB_TITLE, page_icon=ENCORD_ICON
 set_page_css()
 st.write(f"# {EXPORT_PAGE_TITLE}")
 
+st.markdown("### Bulk export consensus results for selected files.")
+st.info(
+    "This feature is currently in progress.\n\r "
+    "If you have any questions or need assistance, please feel free to contact us at support@encord.com!"
 )
