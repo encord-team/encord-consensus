2e1c4417bf20b489878d8b63f9d9870fab8d5a71
alex-encord <alexandre@encord.com>
2024-01-04 18:20:16 +0100

    Add Consensus Workflows.

---
+++ encord_consensus/app/pages/4_🕸️_Workflows.py
@@ -0,0 +1,50 @@
+import json
+from pathlib import Path
+from typing import Dict
+
+import streamlit as st
+
+from encord_consensus.app.common.constants import (
+    WORKFLOWS_PAGE_TITLE,
+    CONSENSUS_BROWSER_TAB_TITLE,
+    ENCORD_ICON_URL,
+)
+from encord_consensus.app.common.css import set_page_css
+from encord_consensus.app.common.state import State, get_state
+from encord_consensus.lib.workflow_utils import send_to_annotate
+from encord_consensus.lib.utils import get_project_root
+
+
+def render_workflows_page():
+    st.set_page_config(page_title=CONSENSUS_BROWSER_TAB_TITLE, page_icon=ENCORD_ICON_URL)
+    set_page_css()
+    st.write(f"# {WORKFLOWS_PAGE_TITLE}")
+    State.init()
+    user_client = get_state().encord_client
+
+    root_dir = get_project_root()
+    config_path = root_dir.joinpath(Path('.workflow_configs.json'))
+
+    with open(config_path, 'r') as f:
+        wf_config = json.load(f)
+
+    def sync(workflow_name: str, workflow: Dict):
+        with st.spinner(f'Syncing {workflow_name}...'):
+            synced_counter = send_to_annotate(**{'user_client': user_client, **workflow['spec']})
+        st.text(f'Executed {workflow_name}, synced: {synced_counter} items')
+
+    for wf_name, wf in wf_config.items():
+        emp = st.empty()
+        col1, col2 = emp.columns([3, 3])
+        with col1:
+            with st.expander(wf_name):
+                st.text('Source Project:')
+                st.text(wf["meta"]["source_project_name"])
+                st.text('Target Projects:')
+                for target_project_name in wf["meta"]["target_project_names"]:
+                    st.text(target_project_name)
+        col2.button("Sync", key=f"sync_{wf_name}", on_click=sync, args=(wf_name, wf,))
+
+
+if __name__ == "__main__":
+    render_workflows_page()
---
+++ encord_consensus/lib/workflow_utils.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from encord import EncordUserClient
+
+
+def send_to_annotate(
+        user_client: EncordUserClient,
+        source_project_hash: str,
+        target_project_hashes: List[str],
+        stage_filter: str = 'COMPLETE',
+        target_priority: int = 1
+) -> int:
+    source_project = user_client.get_project(source_project_hash)
+    target_projects = [
+        user_client.get_project(p_hash) for p_hash in target_project_hashes
+    ]
+    synced_counter = 0
+    for lr_s in source_project.list_label_rows_v2(workflow_graph_node_title_eq=stage_filter):
+        for target_project in target_projects:
+            lr_t = target_project.list_label_rows_v2(data_hashes=[lr_s.data_hash]).pop()
+            if lr_t.priority == 0.5:
+                if not lr_s.is_labelling_initialised:
+                    lr_s.initialise_labels()
+                lr_t.initialise_labels(include_object_feature_hashes=set(), include_classification_feature_hashes=set())
+                for obj in lr_s.get_object_instances():
+                    lr_t.add_object_instance(obj.copy())
+                for cl in lr_s.get_classification_instances():
+                    lr_t.add_classification_instance(cl.copy())
+                lr_t.set_priority(target_priority)
+                synced_counter += 1
+                lr_t.save()
+    return synced_counter
